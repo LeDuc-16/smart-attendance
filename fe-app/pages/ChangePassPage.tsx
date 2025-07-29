4538412d351f8cabf25c14f844db205c09077a6e
Lê Đức <leduc@LeDucs-Macbook-Pro.local>
2025-07-29 08:41:29 +0700

    Update ux for fe-app

--- fe-app/pages/ChangePassPage.tsx
+++ fe-app/pages/ChangePassPage.tsx
@@ -8,7 +8,7 @@ export const ChangePassPage = () => {
   return (
     <View className="flex-1 items-center justify-center px-4">
       <Text className="mb-4 text-center text-2xl font-bold">Change Password Page</Text>
-      <Text className="text-center text-gray-600">This is where you can change your password.</Text>
+      <Text className="text-center text-gray-600">This is where you can change</Text>
     </View>
   );
 };
