89b274520914ffe3120f9664ae4746626b15ed99
Lê Đức <leduc@LeDucs-Macbook-Pro.local>
2025-08-07 07:32:55 +0700

    add folder component and page

--- fe-web/src/App.tsx
+++ fe-web/src/App.tsx
@@ -1,16 +1,10 @@
+import LoginPage from './pages/LoginPage';
+
+
 function App() {
   return (
     <>
-      <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-        <div className="bg-white p-8 rounded-lg shadow-md">
-          <h1 className="text-3xl font-bold text-blue-600 mb-4">
-            Hello Tailwind CSS!
-          </h1>
-          <p className="text-gray-700">
-            Tailwind CSS đã được cài đặt thành công! 🎉
-          </p>
-        </div>
-      </div>
+    <LoginPage />
     </>
   );
 }
