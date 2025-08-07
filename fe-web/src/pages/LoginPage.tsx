12279b38e53060ac73d5430008974a7e74c20c2e
Lê Đức <leduc@LeDucs-Macbook-Pro.local>
2025-08-07 08:15:28 +0700

    login page

--- fe-web/src/pages/LoginPage.tsx
+++ fe-web/src/pages/LoginPage.tsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 const LoginPage = () => {
   return (
-    <div>LoginP</div>
+    <>
+      <h1>Login</h1>
+      <form>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" placeholder="Enter your email" />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" placeholder="Enter your password" />
+        </div>
+        <button>Login</button>
+      </form>
+    </>
   )
 }
 
