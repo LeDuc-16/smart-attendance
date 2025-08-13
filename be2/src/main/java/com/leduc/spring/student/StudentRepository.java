171272720bfba57ab1994dbf2dc061359f149932
Lê Đức <leduc@MacBookPro.lan>
2025-08-14 04:50:16 +0700

    update studentrepo

--- be2/src/main/java/com/leduc/spring/student/StudentRepository.java
+++ be2/src/main/java/com/leduc/spring/student/StudentRepository.java
@@ -1,10 +1,16 @@
 package com.leduc.spring.student;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
     boolean existsByUserName(String name); // Sửa từ existsByStudentName
     boolean existsByStudentCode(String studentCode);
     boolean existsByUserEmail(String email); // Nếu kiểm tra email
     boolean existsByUserAccount(String account); // Nếu kiểm tra account
+    @Modifying
+    @Query("UPDATE Student s SET s.profileImageId = :profileImageId WHERE s.id = :studentId")
+    void updateProfileImageId(String profileImageId, Long studentId);
+
 }
